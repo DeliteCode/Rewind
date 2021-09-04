e0bdb7fa4754a127f7dcb4a1c2e49b261fb7eac3
DeliteCode <80368625+DeliteCode@users.noreply.github.com>
2021-09-04 02:16:27 -0300

    Rewind update september 04
    Code done

--- js/site.js
+++ js/site.js
@@ -2,3 +2,26 @@ function helloWorld() {
     alert("Hello World!");
 
 }
+
+//get the string from the page
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+//reverse the string
+function reverseString(userString) {
+    let revString = [];
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+
+    }
+    return revString;
+
+}
+//display the reversed string
+function displayString(revString) {
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
+}
